9adda69d5ca140b0852227f32a199a3b555b2f91
KamilZywanowski <kamil.zywanowski@gmail.com>
2020-05-26 16:03:26 +0200

    Corrected calculate_control, changed r1 and r2 to l1/2 and l2/2 respectively

--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -5,20 +5,21 @@ class ManiuplatorModel:
     def __init__(self, Tp):
         self.Tp = Tp
         self.l1 = 0.5
-        self.r1 = 0.01
+        self.r1 = self.l1/2.
         self.m1 = 1.
         self.l2 = 0.5
-        self.r2 = 0.01
+        self.r2 = self.l2/2.
         self.m2 = 1.
         self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
         self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2)
-        self.m3 = 0.5
-        self.r3 = 0.01
+        self.m3 = 1.0
+        self.r3 = 0.3
         self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
 
         self.alfa = self.m1 * self.r1 ** 2 + self.I_1 + self.m2 * (self.l1 ** 2 + self.r2 ** 2) + self.I_2 + self.m3 * (self.l1 ** 2 + self.l2 ** 2) + self.I_3
         self.beta = self.m2 * self.l1 * self.r2 + self.m3 * self.l1 * self.l2
         self.gamma = self.m2 * self.r2 ** 2 + self.I_2 + self.m3 * self.l2 ** 2 + self.I_3
+
 
     def M(self, x):
         """
