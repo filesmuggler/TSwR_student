ee88d0e880f7a956ae8188ede6231d1865e7919e
KamilZywanowski <kamil.zywanowski@gmail.com>
2020-05-26 14:53:50 +0200

    Added m3 influence on the model

--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -12,13 +12,14 @@ class ManiuplatorModel:
         self.m2 = 1.
         self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
         self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2)
-        self.m3 = 0.0
+        self.m3 = 0.5
         self.r3 = 0.01
         self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
 
-        self.alfa = self.m1 * self.r1 ** 2 + self.I_1 + self.m2 * (self.l1 ** 2 + self.r2 ** 2) + self. I_2
-        self.beta = self.m2 * self.l1 * self.r2
-        self.gamma = self.m2 * self.r2 ** 2 + self.I_2
+        self.alfa = self.m1 * self.r1 ** 2 + self.I_1 + self.m2 * (self.l1 ** 2 + self.r2 ** 2) + self.I_2 + self.m3 * (self.l1 ** 2 + self.l2 ** 2) + self.I_3
+        self.beta = self.m2 * self.l1 * self.r2 + self.m3 * self.l1 * self.l2
+        self.gamma = self.m2 * self.r2 ** 2 + self.I_2 + self.m3 * self.l2 ** 2 + self.I_3
+
     def M(self, x):
         """
         Please implement the calculation of the mass matrix, according to the model derived in the exercise
@@ -26,12 +27,12 @@ class ManiuplatorModel:
         """
         q1, q2, q1_dot, q2_dot = x
 
-        m11 = self.alfa + 2*self.beta*np.cos(q2)
-        m12 = self.gamma + self.beta*np.cos(q2)
-        m21 = self.gamma + self.beta*np.cos(q2)
+        m11 = self.alfa + 2 * self.beta * np.cos(q2)
+        m12 = self.gamma + self.beta * np.cos(q2)
+        m21 = self.gamma + self.beta * np.cos(q2)
         m22 = self.gamma
 
-        M = np.array([[m11,m12],[m21,m22]])
+        M = np.array([[m11, m12], [m21, m22]])
 
         return M
 
